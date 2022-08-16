5b80f60dfa26f0883d5eabedf5ddad25444b1b31
Sumesh Kumar Gupta <skgs01@gmail.com>
2022-08-16 15:18:36 +0530

    added comment

--- analysis.py
+++ analysis.py
@@ -1,15 +1,17 @@
 from time import time
 
-
+#defining a function in one way for same problem
 def fun1(n):
 	return n*(n+1)/2
 
+#for the same problem another way to solve the problem
 def fun2(n):
 	sum=0
 	for i in range(1,n+1):
 		sum=sum+i
 	return sum
 
+#for the same problem another way to solve the problem
 def fun3(n):
 	sum=0
 	for i in range(1,n+1):
